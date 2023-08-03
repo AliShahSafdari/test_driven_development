c777a72c69e6ba35d5543eb7ca045d70dabf05a2
Nasir <nasirbhatti.nk@gmail.com>
2023-08-03 21:55:44 +0500

    Add tests for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe Solver do
       expect(Solver.reverse('ruby')).to eq('ybur')
     end
   end
+  describe '#fizzbuzz' do
+    it 'returns "fizz" when the number is divisible by 3' do
+      expect(Solver.fizzbuzz(9)).to eq('fizz')
+      expect(Solver.fizzbuzz(12)).to eq('fizz')
+    end
+    it 'returns "buzz" when the number is divisible by 5' do
+      expect(Solver.fizzbuzz(10)).to eq('buzz')
+      expect(Solver.fizzbuzz(25)).to eq('buzz')
+    end
+    it 'returns "fizzbuzz" when the number is divisible by both 3 and 5' do
+      expect(Solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(Solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+    it 'returns the number as a string for any other case' do
+      expect(Solver.fizzbuzz(7)).to eq('7')
+      expect(Solver.fizzbuzz(13)).to eq('13')
+    end
+  end
 end
